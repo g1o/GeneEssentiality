5020cac0a426f0588dcf85f40fe18d20b2764aa0
Giovanni Marques de Castro <13785663+g1o@users.noreply.github.com>
2019-11-11 17:39:55 -0300

    removed plyr and some bugfixes
    plyr had a count function that conflicted with the count from seqinR.

--- Features_calc/Read_and_process_Test_set.R
+++ Features_calc/Read_and_process_Test_set.R
@@ -1,65 +1,61 @@
 #!/bin/env R
-library(plyr) #count function conflicts with the one in seqinR. This library must be loaded before seqinR
+library(data.table) #count function conflicts with the one in seqinR. This library must be loaded before seqinR
 library(seqinr)
 library(parallel)
 library(doMC)
 library(pROC)
 library(caret)
 library(protr)
+script.dir<-dirname(sys.frame(1)$ofile) 
+FEATURES_func<-paste0(script.dir,"/functions/Serial_gene_features_extraction.R") #when sourcing, get path to function relative to this script
+source(FEATURES_func,echo=T)
 
+CPU=30
 load("nicemodels_ntree250_07AUC.RData") # LOAD TRAINED MODELS list # need to be changed
 PFAM_path="/mnt/Databases/PFAM/Pfam-A.hmm"; #change the path to your hmmpressed pfam database
-ge_cripr<-read.fasta("../data/drosophila/GE.fasta")
-gne_cripr<-read.fasta("../data/drosophila/GNE.fasta")
-
-#TcGNE<-read.fasta("Tribolium_NonEssential-less_than0.30lethal_from_ibeetle.fasta")
-#TcGE<-read.fasta("Tribolium_top100_RNAiLethal_FROM_Ulrich2015_allCDSs_from_genes.fasta")
+GE<-read.fasta("../data/drosophila/GE.fasta")
+GNE<-read.fasta("../data/drosophila/GNE.fasta")
 
 LAMBDA=length(grep( "Xc2.lambda" , colnames( modellist[[1]]$trainingData ))); #set like the training data in model
 OMEGA=0.05;#set like the training data in model
-source("functions/Serial_gene_features_extraction.R",echo=T)
 
-cl<-makeCluster(6,type="FORK")
-fcGE <-rbind.fill(parSapply(cl,ge_cripr ,function (x) Calc_feats(x,PFAM_PATH=PFAM_path,LAMBDA=LAMBDA,OMEGA=OMEGA)))
-fcGNE<-rbind.fill(parSapply(cl,gne_cripr,function (x) Calc_feats(x,PFAM_PATH=PFAM_path,LAMBDA=LAMBDA,OMEGA=OMEGA)))
+cl<-makeCluster(CPU,type="FORK")
+#-------------------------------------------------
+Features_essential<-as.data.frame(rbindlist(
+                                  parSapply(cl,GE,function(gene)
+                                            Calc_feats(gene,PFAM_PATH=PFAM_path,LAMBDA=LAMBDA,OMEGA=OMEGA)),
+                    fill=T,idcol=T))
 
-fcGE<- data.frame(fcGE,Class="E")   #Set when outcome is known
-fcGNE<- data.frame(fcGNE,Class="NE")#Set when outcome is known
+rownames(Features_essential)<-t(Features_essential[,1])
+Features_essential[is.na(Features_essential)] <- F #binnary NA to False
+Features_essential<- data.frame(Features_essential,Class="E")
 
+Features_notessential<-as.data.frame(rbindlist(
+                                     parSapply(cl,GNE,function(gene)
+                                               Calc_feats(gene,PFAM_PATH=PFAM_path,LAMBDA=LAMBDA,OMEGA=OMEGA)),
+                       fill=T,idcol=T))
+
+rownames(Features_notessential)<-t(Features_notessential[,1])
+Features_notessential[is.na(Features_notessential)] <- F
+Features_notessential<-data.frame(Features_notessential,Class="NE")
+
+Complete_set<-rbindlist(list(Features_essential,Features_notessential),fill=T,idcol=T)
+Complete_set<-as.data.frame(Complete_set)
+rownames(Complete_set)<-t(Complete_set[,2])
+Complete_set<-Complete_set[,-c(1,2)]
+Complete_set[is.na(Complete_set)] <- F
+
+#-------------------------------------------------
 stopCluster(cl)
-Crispr_set<-rbind.fill(list(fcGE,fcGNE))
-
-ntr<-setdiff(names(modellist[[1]]$trainingData),names(Crispr_set))
+ntr<-setdiff(names(modellist[[1]]$trainingData),names(Complete_set))
 ntr<-ntr[which(ntr!=".outcome")]
-nte<-setdiff(names(Crispr_set),names(modellist[[1]]$trainingData))
+nte<-setdiff(names(Complete_set),names(modellist[[1]]$trainingData))
 nte<-nte[which(nte!="Class")]
 
+Complete_set[,nte]<-NULL
+Complete_set[,ntr]<-FALSE
+Complete_set[is.na(Complete_set)] <- F
 
-Crispr_set[,nte]<-NULL
-Crispr_set[,ntr]<-FALSE
-Crispr_set[is.na(Crispr_set)] <- F
+res<-predict(modellist[[1]],Complete_set,type="prob");
+result.roc.crispr<-roc(Complete_set$Class,res$E)
 
-res<-predict(modellist[[1]],Crispr_set,type="prob");
-result.roc.crispr<-roc(Crispr_set$Class,res$E)
-
-
-#cl<-makeCluster(6,type="FORK")
-#fcGE_Tc <-rbind.fill(parSapply(cl,TcGE ,function (x) Calc_feats(x,LAMBDA=LAMBDA,OMEGA=OMEGA))) #tribolium Features Calc
-#fcGNE_Tc<-rbind.fill(parSapply(cl,TcGNE,function (x) Calc_feats(x,LAMBDA=LAMBDA,OMEGA=OMEGA)))
-## 
-#fcGE_Tc<- data.frame(fcGE_Tc,Class="E")  #Set when outcome is known
-#fcGNE_Tc<- data.frame(fcGNE_Tc,Class="NE")#Set when outcome is known
-# 
-#stopCluster(cl)
-#Tribolium_set<-rbind.fill(list(fcGE_Tc,fcGNE_Tc))
-# 
-#ntr<-setdiff(names(modellist[[1]]$trainingData),names(Tribolium_set))
-#ntr<-ntr[which(ntr!=".outcome")]
-#nte<-setdiff(names(Tribolium_set),names(modellist[[1]]$trainingData))
-#nte<-nte[which(nte!="Class")]
-#
-# 
-#Tribolium_set[,nte]<-NULL
-#Tribolium_set[,ntr]<-FALSE
-#Tribolium_set[is.na(Tribolium_set)] <- F
-#
