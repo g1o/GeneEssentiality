7bcca66faa177c0061779a7c147af230c7e15221
Giovanni Marques de Castro <13785663+g1o@users.noreply.github.com>
2019-12-13 11:39:03 -0300

    Create Plot_model_ROC.R

---
+++ Plot_model_ROC.R
@@ -0,0 +1,7 @@
+#!/bin/env R
+#Plots the ROC from the model cross-validation 
+anot<-paste0('\n',paste0(label[1,1:2],collapse=" AUC="),'\n',paste0(label[2,1:2],collapse=" AUC="),'\n',paste0(label[3,1:2],collapse=" AUC="),'\n',paste0(label[4,1:2],collapse=" AUC="),'\n')
+
+g<-ggplot(modellist[[1]]$pred,aes(m = NE, d = obs, color=factor(mtry))) + geom_roc(n.cut=0) + style_roc(theme=theme_classic,ylab="Sensibilidade (Taxa de verdadeiros positivos)",xlab="Taxa de falsos positivos (Especificidade - 1)") + annotate(geom="text",x = 0.7, y = 0.2, label=c(anot))+ geom_abline(intercept=0, slope=1, colour="orange")  ;g;dev.off()
+
+g; dev.off()
